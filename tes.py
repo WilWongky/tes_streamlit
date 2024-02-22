db8839384b6bb114ed7b82a5b59b245f0a8d536d
WilWongky <160838504+WilWongky@users.noreply.github.com>
2024-02-22 16:15:46 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("HITUNG LUAS PERSEGI PANJANG")
-panjang = st.number_input("Masukkan Nilai Lebar", 0)
+panjang = st.number_input("Masukkan Nilai Panjang", 0)
 lebar = st.slider ("Tentukan Nilai Lebar", 0, 100, 1, 1)
 hitung = st.button("Hitung Luas")
 
