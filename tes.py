2125bfa0ee2c709827be176edd4dee994800adda
WilWongky <160838504+WilWongky@users.noreply.github.com>
2024-02-22 15:53:20 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("HITUNG LUAS PERSEGI PANJANG")
+panjang = st.number_input("Masukkan Nilai Lebar", 0)
+lebar = st.slider ("Tentukan Nilai Lebar", 0, 100, 1, 1)
+hitung = st.button("Hitung Luas")
+
+   
+if hitung :
+    luas = panjang * lebar
+    st.success (f"Luas Persegi Panjang = {luas}")
+    
+            
